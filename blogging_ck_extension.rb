015d15219fb8cfcab745b47d370be9b077a80c22
x@ES <KEIvanov@gmail.com>
2010-06-02 17:49:07 +0400

    Renaming extension

--- blogging_ck_extension.rb
+++ blogging_ck_extension.rb
@@ -1,10 +1,10 @@
 # Uncomment this if you reference any of your controllers in activate
 # require_dependency 'application'
 
-class BloggingSpreeExtension < Spree::Extension
+class BloggingCkExtension < Spree::Extension
   version "0.2.1"
-  description "BloggingSpree: A Spree blogging solution"
-  url "git://github.com/jaymendoza/spree-blogging-spree.git"
+  description "BloggingSpree: A Spree blogging solution. Aimed to using with CKEditor."
+  url "http://github.com/x2es/spree-blogging-ck"
 
   def activate
   end
